e8ece54e2c8ce057264646782af77612a00388e0
successive-dev <vishal.tewatia@successive.tech>
2019-04-16 17:41:45 +0530

    fixed untracked files

--- build/src/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var post_1 = require("./post");
-exports.post = post_1.post;
-exports.postRouter = post_1.postRouter;
--- build/src/repositories/post/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PostModel_1 = require("./PostModel");
-exports.Post = PostModel_1.Post;
-var PostSchema_1 = require("./PostSchema");
-exports.PostSchema = PostSchema_1.default;
-var PostRepository_1 = require("./PostRepository");
-exports.PostRepo = PostRepository_1.default;
